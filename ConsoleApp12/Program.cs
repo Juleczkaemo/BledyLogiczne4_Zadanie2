6ccaf86da4086a2fb5c2075dec24dbe8082e9a43
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-11-27 09:03:53 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string tekst = "programowanie";
+        string odwrocony = "";
+
+        for (int i = tekst.Length; i >= 0; i--)
+        {
+            odwrocony += tekst[i];
+        }
+
+        Console.WriteLine(odwrocony);
+    }
+}
